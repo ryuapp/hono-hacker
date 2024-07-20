8302fc05648f4a8fd0c7615afa256f41bf04c891
ryu <114303361+ryuapp@users.noreply.github.com>
2024-07-20 13:05:36 +0900

    Update site url

--- config/site.ts
+++ config/site.ts
@@ -1,3 +1,3 @@
-export const SITE_URL: string = "";
+export const SITE_URL: string = "https://hono-hacker-news.deno.dev";
 export const SITE_TITLE = "Hono Hacker News";
 export const DESCRIPTION = "Hacker News clone made with Hono";
