ebf23750b00cdb9e37f992e14a8f95f5ff9171c0
ryu <114303361+ryuapp@users.noreply.github.com>
2024-07-21 01:06:49 +0900

    Improve ranking design

--- components/ItemSummary.tsx
+++ components/ItemSummary.tsx
@@ -8,34 +8,38 @@ type ItemSummaryProps = {
 export function ItemSummary(props: ItemSummaryProps) {
   const { item, rank } = props;
   return (
-    <div>
-      <div class="my-1">
+    <div class="flex my-1">
+      <div class="flex mt-1">
         {rank
           ? (
             <span class="text-sm text-gray-500 mr-1">
-              {rank < 10 ? <>&nbsp;</> : ""}
+              {rank < 10 ? <>&nbsp;&nbsp;</> : ""}
               {rank}.
             </span>
           )
           : null}
-        <span class="cursor-pointer mr-1 text-xs text-gray-400">▲</span>
-        <span class="mr-1 text-sm">
-          <a href={getUrl(item)}>{item.title}</a>
-        </span>
-        <span class="text-xs text-gray-500">
-          {item.url ? `(${host(item.url)})` : ""}
-        </span>
+        <span class="cursor-pointer text-sm text-gray-400 mr-1">▲</span>
       </div>
-      <div class="text-xs text-gray-500">
-        {item.points} point{item.points > 1 ? "s" : ""} by{" "}
-        <a class="hover:underline" href={`/user?id=${item.user}`}>
-          {item.user}
-        </a>{" "}
-        {timeAgo(item.time)} ago |{" "}
-        <a class="hover:underline" href={`/item?id=${item.id}`}>
-          {`${item.comments_count} comment` +
-            (item.comments_count && item.comments_count > 1 ? "s" : "")}
-        </a>
+      <div>
+        <div>
+          <span class="mr-1 text-sm">
+            <a href={getUrl(item)}>{item.title}</a>
+          </span>
+          <span class="text-xs text-gray-500">
+            {item.url ? `(${host(item.url)})` : ""}
+          </span>
+        </div>
+        <div class="text-xs text-gray-500">
+          {item.points} point{item.points > 1 ? "s" : ""} by{" "}
+          <a class="hover:underline" href={`/user?id=${item.user}`}>
+            {item.user}
+          </a>{" "}
+          {timeAgo(item.time)} ago |{" "}
+          <a class="hover:underline" href={`/item?id=${item.id}`}>
+            {`${item.comments_count} comment` +
+              (item.comments_count && item.comments_count > 1 ? "s" : "")}
+          </a>
+        </div>
       </div>
     </div>
   );
