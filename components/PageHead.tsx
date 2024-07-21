9994a3558429508368c215e1fd6dad2a121832ea
ryu <114303361+ryuapp@users.noreply.github.com>
2024-07-21 09:19:18 +0900

    Add og image

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -19,11 +19,11 @@ export function PageHead({
       <meta property="og:description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content={SITE_URL + "/ogp.png"} />
+      <meta property="og:image" content={SITE_URL + "/static/og.png"} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={SITE_URL + "/ogp.png"} />
+      <meta name="twitter:image" content={SITE_URL + "/static/og.png"} />
     </>
   );
 }
