e1021bced951d10f3e960d42a29ab226971e431f
ryu <114303361+ryuapp@users.noreply.github.com>
2024-08-15 14:43:27 +0900

    Fix to display for mobile

--- layouts/BaseLayout.tsx
+++ layouts/BaseLayout.tsx
@@ -6,6 +6,8 @@ export function BaseLayout(children: Child) {
   return (
     <html>
       <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/static/logo.svg" />
         <link rel="stylesheet" type="text/css" href="/static/main.gen.css" />
       </head>
