6496e4f654a8f168037997583e6a0d7e10ee5332
Ryu <114303361+ryuapp@users.noreply.github.com>
2026-04-02 19:19:24 +0900

    Add copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,14 +2,20 @@ import { GitHubIcon } from "./github-icon.tsx";
 
 export function Footer() {
   return (
-    <footer class="border-orange-200 border-t-2 bg-stone-100">
-      <div class="flex justify-center gap-4 py-4">
+    <footer class="border-orange-200 border-t-2 bg-stone-100 flex flex-col items-center gap-2">
+      <div class="flex gap-4 pt-4">
         <a
           class="flex items-center"
           href="https://github.com/ryuapp/hono-hacker"
           aria-label="Repository"
         >
           <GitHubIcon />
+        </a>
+      </div>
+      <div class="pb-4 text-center text-sm text-stone-600">
+        © 2026{" "}
+        <a class="underline hover:no-underline" href="https://ryu.app">
+          Ryu
         </a>
       </div>
     </footer>
